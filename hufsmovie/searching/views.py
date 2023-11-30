a8bed859047a5b3e0c6391b77a45370d760a2d4e
ShinSub98 <wjdtjq26@naver.com>
2023-11-30 13:56:39 +0900

    recommend

--- hufsmovie/searching/views.py
+++ hufsmovie/searching/views.py
@@ -5,8 +5,6 @@ import json
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from scipy import spatial
-import operator
-import csv
 import os
 
 # Create your views here.
@@ -15,75 +13,75 @@ file_path = os.path.join(script_dir, 'new_movie_list.csv')
 
 movies = pd.read_csv(file_path)
 
-def Similarity(movieId1, movieId2):
-    a = movies.iloc[movieId1]
-    b = movies.iloc[movieId2]
+# def Similarity(movieId1, movieId2):
+#     a = movies.iloc[movieId1]
+#     b = movies.iloc[movieId2]
     
-    genresA = a['binary_genres']
-    genresB = b['binary_genres']
+#     genresA = a['binary_genres']
+#     genresB = b['binary_genres']
     
-    genreDistance = spatial.distance.cosine(genresA, genresB)
+#     genreDistance = spatial.distance.cosine(genresA, genresB)
     
-    castA = a['binary_cast']
-    castB = b['binary_cast']
-    castDistance = spatial.distance.cosine(castA, castB)
+#     castA = a['binary_cast']
+#     castB = b['binary_cast']
+#     castDistance = spatial.distance.cosine(castA, castB)
     
-    directA = a['binary_director']
-    directB = b['binary_director']
-    directDistance = spatial.distance.cosine(directA, directB)
+#     directA = a['binary_director']
+#     directB = b['binary_director']
+#     directDistance = spatial.distance.cosine(directA, directB)
     
-    wordsA = a['binary_words']
-    wordsB = b['binary_words']
-    wordsDistance = spatial.distance.cosine(wordsA, wordsB)
+#     wordsA = a['binary_words']
+#     wordsB = b['binary_words']
+#     wordsDistance = spatial.distance.cosine(wordsA, wordsB)
     
-    return genreDistance + directDistance + castDistance + wordsDistance
+#     return genreDistance + directDistance + castDistance + wordsDistance
 
 
-def recommend_movie(name):
-    new_movie = movies[movies['original_title'].str.contains(name)].iloc[0].to_frame().T
+# def recommend_movie(name):
+#     new_movie = movies[movies['original_title'].str.contains(name)].iloc[0].to_frame().T
     
-    print('Selected Movie: ', new_movie.original_title.values[0])
+#     print('Selected Movie: ', new_movie.original_title.values[0])
     
-    def getNeighbors(baseMovie, K):
-        distances = []
+#     def getNeighbors(baseMovie, K):
+#         distances = []
 
 
-        for index, movie in movies.iterrows():
-            if movie['new_id'] != baseMovie['new_id'].values[0].astype(int):
-                dist = Similarity(baseMovie['new_id'].values[0], movie['new_id'])
-                distances.append((movie['new_id'], dist))
-        distances.sort(key=operator.itemgetter(1))
-        neighbors = []
+#         for index, movie in movies.iterrows():
+#             if movie['new_id'] != baseMovie['new_id'].values[0].astype(int):
+#                 dist = Similarity(baseMovie['new_id'].values[0], movie['new_id'])
+#                 distances.append((movie['new_id'], dist))
+#         distances.sort(key=operator.itemgetter(1))
+#         neighbors = []
 
-        for x in range(min(K, len(distances))):
-            neighbors.append(distances[x])
-        return neighbors
+#         for x in range(min(K, len(distances))):
+#             neighbors.append(distances[x])
+#         return neighbors
     
-    K = 10
-    neighbors = getNeighbors(new_movie, K)
-    data = []
+#     K = 10
+#     neighbors = getNeighbors(new_movie, K)
+#     data = []
 
-    for neighbor in neighbors:
-        m = {
-            "title" : str(movies.iloc[neighbor[0]][1]),
-            "genres" : str(movies.iloc[neighbor[0]][2]).strip('[]').replace(' ', ''),
-            "rating" : str(movies.iloc[neighbor[0]][4])
-        }
-        data.append(m)
-        # print(
-        # str(movies.iloc[neighbor[0]][1]) +
-        # " | Genres: " +
-        # str(movies.iloc[neighbor[0]][2]).strip('[]').replace(' ', '') +
-        # " | Rating: " +
-        # str(movies.iloc[neighbor[0]][4]))
+#     for neighbor in neighbors:
+#         m = {
+#             "title" : str(movies.iloc[neighbor[0]][1]),
+#             "genres" : str(movies.iloc[neighbor[0]][2]).strip('[]').replace(' ', ''),
+#             "rating" : str(movies.iloc[neighbor[0]][4])
+#         }
+#         data.append(m)
+#         # print(
+#         # str(movies.iloc[neighbor[0]][1]) +
+#         # " | Genres: " +
+#         # str(movies.iloc[neighbor[0]][2]).strip('[]').replace(' ', '') +
+#         # " | Rating: " +
+#         # str(movies.iloc[neighbor[0]][4]))
 
-    return data
+#     return data
 
 
-class Recommend(APIView):
-    def get(self, request, movie_title):
-        res = recommend_movie(movie_title)
-        return Response(res)
+# class Recommend(APIView):
+#     def get(self, request, movie_title):
+#         res = recommend_movie(movie_title)
+#         return Response(res)
     
 
 
@@ -97,4 +95,44 @@ class Search(APIView):
         res = {
             "data" : title_list
         }
+        return Response(res)
+    
+
+class Recommend(APIView):
+    def post(self, request, movie_title):
+        script_dir = os.path.dirname(__file__)
+        file_path = os.path.join(script_dir, 'recommend_movie.csv')
+        rec_movie = pd.read_csv(file_path)
+
+
+        aa = rec_movie[rec_movie['original_title'] == movie_title]['recommend_movie'].values[0]
+
+        lines = aa.splitlines()
+
+        del lines[0]
+        del lines[0]
+
+        data = []
+
+        for line in lines:
+            str = line.split('|')
+            genres = str[1].replace("Genres: ", "").replace("'", "").strip().split(',')
+            genres_str = ""
+            for g in genres:
+                genres_str += g
+                genres_str += ", "
+
+
+            m = {
+                "title" : str[0].strip(),
+                "genres" : genres_str[:-2],
+                "rating" : str[2].replace("Rating: ", "").strip()
+            }
+            data.append(m)
+
+        res = {
+            "data" : data
+        }
+
+
         return Response(res)
